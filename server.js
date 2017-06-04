e0b665c0ab9bd05e3fd5337f3485e3a4601eac7c
satish <satish_cards@yahoo.com>
2017-06-04 15:36:18 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const fs=require('fs');
+const hbs=require('hbs');
+hbs.registerPartials(__dirname+'/partials');
+var app=express();
+
+app.set('view engine','hbs');
+app.use((request,response,next)=>{
+
+    var now=new Date().getFullYear();
+    log=(`${now} : ${request.method}  ${request.url}`);
+    console.log(log);
+    fs.appendFile('server.log',log,(err)=>{
+        if(err){
+            console.log('unable to load file');
+        }
+        
+    });
+    next();
+  /*  response.render('home',{
+         pageTitle:'Smartchip Technologies',
+         pageHeader:'Smartchip Technologies',
+         welcomeMessage:'Welcome to Smartchip Technologies',
+         currentYear: new Date().getFullYear()
+
+
+     });*/
+});
+//app.use(express.static(__dirname+'/public'));
+ app.get('/',(request,response)=>{
+     response.render('home',{
+         pageTitle:'Smartchip Technologies',
+         pageHeader:'Smartchip Technologies',
+         welcomeMessage:'Welcome to Smartchip Technologies',
+         currentYear: new Date().getFullYear()
+
+
+     });
+    
+    // response.send({
+    //     name:'satish kumar K V',
+    //     address:{
+    //         street:'santhapet',
+    //         city:'nellore'
+    //     }
+    // });
+ });
+ app.get('/about',(request,response)=>{
+     response.render('about',{
+        pageTitle:'Smartchip Tehcnologies',
+        pageHeader:'About Page',
+        currentYear:new Date().getFullYear()
+
+     });
+   // response.send('About Smartchip Technologies');
+ });
+ app.get('/bad',(request,response)=>{
+    response.send({
+        
+            error:'Unable to  Process the Request'
+        });
+   
+ });
+ app.listen(3000,()=>{
+     console.log('Server Listening On port 3000');
+ });
